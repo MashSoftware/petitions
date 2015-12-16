4882ef8d7e0bb62dc2a1018d5a0d6bb848543d6c
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2015-12-16 01:14:44 +0000

    order and rank contries and constituencies by signatory count

--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,7 @@ import requests
 from application import app
 from application.utils import get_mp_data
 from flask import render_template
+from operator import itemgetter
 
 @app.route('/', methods=["GET"])
 def index():
@@ -20,4 +21,11 @@ def petition(id):
     url = 'https://petition.parliament.uk/petitions/' + id + '.json'
     r = requests.get(url)
     data = json.loads(r.text)
-    return render_template('petition.html', data=data)
+
+    countries = data['data']['attributes']['signatures_by_country']
+    sorted_countries = sorted(countries, key=itemgetter('signature_count'), reverse=True)
+
+    constituencies = data['data']['attributes']['signatures_by_constituency']
+    sorted_constituencies = sorted(constituencies, key=itemgetter('signature_count'), reverse=True)
+
+    return render_template('petition.html', data=data, countries=sorted_countries, constituencies=sorted_constituencies)
