5d0c1b3d53ece23d926ce6e09aafbe8600cd3d2f
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-01-15 18:43:36 +0000

    petitions pagination

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,7 @@ import json
 import requests
 from application import app
 from application.utils import get_mp_data, constituency_extent, constituency_collection
-from flask import render_template
+from flask import render_template, request
 from operator import itemgetter
 
 @app.route('/', methods=["GET"])
@@ -11,10 +11,14 @@ def index():
 
 @app.route('/petitions', methods=["GET"])
 def petitions():
-    url = 'https://petition.parliament.uk/petitions.json'
+    if request.args.get('page'):
+        url = 'https://petition.parliament.uk/petitions.json?page=' + request.args.get('page')
+    else:
+        url = 'https://petition.parliament.uk/petitions.json'
+    page = request.args.get('page')
     r = requests.get(url)
     data = json.loads(r.text)
-    return render_template('petitions.html', data=data)
+    return render_template('petitions.html', data=data, page=page)
 
 @app.route('/petitions/<id>', methods=["GET"])
 def petition(id):
@@ -43,7 +47,7 @@ def map(id):
     data = json.loads(r.text)
 
     constituencies = data['data']['attributes']['signatures_by_constituency']
-    extent = json.dumps(constituency_extent('E14000879'))
+    #extents = constituency_collection(constituencies)
 
-    return render_template('map.html',
-        extents=extent)
+    return render_template('map.html')
+        #extents=extents)
