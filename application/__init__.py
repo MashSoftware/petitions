6284c5a7047754c915256e2c513375767189f8e6
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-06-29 14:08:45 +0100

    flask compress

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,12 @@
 # flake8: noqa
 from flask import Flask
 from flask_assets import Environment, Bundle
+from flask_compress import Compress
 
 app = Flask(__name__)
+
+#App config
+app.config.from_pyfile('config.py')
 
 # Flask Assets
 assets = Environment(app)
@@ -11,6 +15,7 @@ js = Bundle('js/map.js', filters='jsmin', output='js/map.min.js')
 assets.register('custom_css', css)
 assets.register('map_js', js)
 
-app.config.from_pyfile('config.py')
+# Flask Compress
+Compress(app)
 
 import application.views
