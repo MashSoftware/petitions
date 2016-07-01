92306d52e96ec48c11419f291ecfc152a81ee351
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-07-01 09:47:00 +0100

    remove flask assets from init

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 from flask import Flask
-from flask_assets import Environment, Bundle
 from flask_compress import Compress
 from flask_cache import Cache
 
@@ -8,11 +7,6 @@ app = Flask(__name__)
 
 #App config
 app.config.from_pyfile('config.py')
-
-# Flask Assets
-assets = Environment(app)
-js = Bundle('js/map.js', filters='jsmin', output='js/map.min.js')
-assets.register('map_js', js)
 
 # Flask Compress
 Compress(app)
